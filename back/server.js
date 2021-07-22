1828bddb9c7342aca22968af0ebfbaa3ee562955
Otoniel Carlos <otonielcarlosf@gmail.com>
2021-07-21 23:10:50 -0500

    hunting orders_v2 notifications only

--- back/server.js
+++ back/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 const { db } = require('./src/db');
 // const { getOrders } = require('./src/func');
-const { sendMessage } = require('./src/message');
+// const { sendMessage } = require('./src/message');
 
 app.use(cors());
 app.use(express.json());
@@ -12,10 +12,13 @@ const PORT = process.env.PORT || 4000;
 
 app.post('/callbacks', async (req, res) => {
   try {
+
     const { resource, topic } = req.body
       res.status(200).send();
-      // await sendMessage(resource);
-console.log(req.body);
+      if(topic === 'orders_v2'){
+        // await sendMessage(resource);
+        console.log(req.body);
+      }
 
   } catch (error) {
     console.log(error);
