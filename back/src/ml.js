c8aaf090a98f683d45f1481b80c43d1f7daacd39
Otoniel Carlos <otonielcarlosf@gmail.com>
2021-12-13 10:13:56 -0600

    added name2 for lastname

--- back/src/ml.js
+++ back/src/ml.js
@@ -1,17 +1,17 @@
 const axios = require('axios');
 const log = console.log
 const body = {
-  grant_type: 'refresh_token',
-  client_id: '2796079999742920',
-  client_secret: 'iVV5i9dJyklUQoFwgxP83H8EqdmdZhFN',
-  refresh_token: 'TG-60c57d748430ea00073c61f4-766642543',
+  "grant_type": "refresh_token",
+  "client_id": "2796079999742920",
+  "client_secret": "iVV5i9dJyklUQoFwgxP83H8EqdmdZhFN",
+ "refresh_token": "TG-61b77106eae786001b26a457-766642543"
 };
 const body_1 = {
-  grant_type: 'authorization_code',
-  client_id: '2796079999742920',
-  client_secret: 'iVV5i9dJyklUQoFwgxP83H8EqdmdZhFN',
-  code: 'TG-60c57d65e7c3ac00075d768b-766642543',
-  redirect_uri: 'https://appleamaze.herokuapp.com/',
+  "grant_type": "authorization_code",
+  "client_id": "2796079999742920",
+  "client_secret": "iVV5i9dJyklUQoFwgxP83H8EqdmdZhFN",
+  "code": "TG-61b770c171f899001bb1ee77-766642543",
+  "redirect_uri": "https://appleamaze.herokuapp.com/",
 };
 
 const token = () => {
