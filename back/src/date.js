d6cc091c2345139265da02b48820a7ef6b01757d
Otoniel Carlos <otonielcarlosf@gmail.com>
2021-12-20 11:29:52 -0600

    new

--- back/src/date.js
+++ back/src/date.js
@@ -1,21 +1,23 @@
 const axios = require('axios');
 const { token } = require('./ml');
-
+const log = console.log;
 const getDate = async resource => {
   try {
     let accessToken = await token();
     let resDate = await axios.get(`https://api.mercadolibre.com/${resource}`, {
       headers: { Authorization: `Bearer ${accessToken}` },
     });
+
     let dateCreated = resDate.data.date_created;
+    //log(dateCreated);
     let fullDate = dateCreated.slice(0, 10);
-    console.log(fullDate);
+    //log(fullDate);
     return fullDate;
   } catch (error) {
-    console.log('error getting date in date.js file');
+    log('error getting date in date.js file');
   }
 };
-// getDate('orders/5081792652');
+getDate('orders/5081792652');
 module.exports = {
   getDate,
 };
--- back/src/ml.js
+++ back/src/ml.js
@@ -1,17 +1,17 @@
 const axios = require('axios');
-const log = console.log
+const log = console.log;
 const body = {
-  "grant_type": "refresh_token",
-  "client_id": "2796079999742920",
-  "client_secret": "iVV5i9dJyklUQoFwgxP83H8EqdmdZhFN",
- "refresh_token": "TG-61b77106eae786001b26a457-766642543"
+  grant_type: 'refresh_token',
+  client_id: '2796079999742920',
+  client_secret: 'iVV5i9dJyklUQoFwgxP83H8EqdmdZhFN',
+  refresh_token: 'TG-61b77106eae786001b26a457-766642543',
 };
 const body_1 = {
-  "grant_type": "authorization_code",
-  "client_id": "2796079999742920",
-  "client_secret": "iVV5i9dJyklUQoFwgxP83H8EqdmdZhFN",
-  "code": "TG-61b770c171f899001bb1ee77-766642543",
-  "redirect_uri": "https://appleamaze.herokuapp.com/",
+  grant_type: 'authorization_code',
+  client_id: '2796079999742920',
+  client_secret: 'iVV5i9dJyklUQoFwgxP83H8EqdmdZhFN',
+  code: 'TG-61b770c171f899001bb1ee77-766642543',
+  redirect_uri: 'https://appleamaze.herokuapp.com/',
 };
 
 const token = () => {
@@ -26,7 +26,7 @@ const token = () => {
     .catch(err => console.log(err));
 };
 
-// token();
+token();
 module.exports = {
   token,
 };
