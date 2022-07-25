06aa98ab13ccbaa349a20514dfefe6b9150b4402
Otoniel Carlos <otonielcarlosf@gmail.com>
2022-07-25 17:44:29 -0500

    commited

--- back/server.js
+++ back/server.js
@@ -147,9 +147,9 @@ app.delete('/borrar/:guia', async (req, res) => {
   }
 })
 
-// app.get('/*', (req, res) => {
-// 	res.sendFile(path.join(__dirname, 'build/index.html'))
-// })
+app.get('/*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'build/index.html'))
+})
 
 app.listen(PORT, err => {
 	if (err) {
