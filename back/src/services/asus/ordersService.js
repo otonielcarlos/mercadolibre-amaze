19b0c5a1bfe280a51e487e50be1263afaf2bb113
Otoniel Carlos <otonielcarlosf@gmail.com>
2022-10-05 19:54:10 -0500

    build front

--- back/src/services/asus/ordersService.js
+++ back/src/services/asus/ordersService.js
@@ -62,7 +62,7 @@ async function getAsusInformationOrdersFromDates(yesterday, today) {
 }
 
 async function updateAsusOrderStatusFactura(order_id) {
-  const isUpdated = await statusUpdateAsus({order: order_id, status: 'invoiceUpdated', comment: 'Factura Enviada al correo ', notify: 1})
+  const isUpdated = await statusUpdateAsus({order: order_id, status: 'InvoiceUploaded', comment: 'Factura Enviada al correo ', notify: 1})
   const isDisable = await updateFacturaStatus(order_id)
   return 
 }
